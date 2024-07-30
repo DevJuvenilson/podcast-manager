77a937504ed113f9185cdb8a0022aa3ee152cee7
Daniel Ribeiro <109773063+DevJuvenilson@users.noreply.github.com>
2024-07-30 14:30:22 -0300

    create repositories
    Created repositories for a model of interface for the json file

--- src/services/list-episodes-service.ts
+++ src/services/list-episodes-service.ts
@@ -1,18 +1,7 @@
+import { repositoryPodcast } from "../repositories/podcasts-repository";
+
 export const serviceListEpisodes = async () => {
-    const data = [
-        {
-            podcastName: "Flow",
-            episodeName: "CBUM - Flow #319",
-            videoId: "pQSuQmUfS3",
-            categories: ["saúde", "esporte", "bodybuilding"]
-        },
-        {
-            podcastName: "Flow",
-            episodeName: "RUBENS BARRICHELLO - Flow #339",
-            videoId: "4KDGTdiOV4I",
-            categories: ["esporte", "corrida"]
-        },
-    ];
+    const data = await repositoryPodcast();
 
     return data;
 };
