eddaa2fd1a9350c55cb81ea10b83a4b586652c5d
Daniel Ribeiro <109773063+DevJuvenilson@users.noreply.github.com>
2024-08-03 18:02:11 -0300

    create app.ts

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,11 @@
 import * as http from "http";
-import { getFilterEpisodes, getListEpisodes } from "./controllers/podcasts-controller";
-import { Routes } from "./routes/routes";
-import { HttpMethod } from "./utils/http-methods";
+import { app } from "./app";
 
 //CRIANDO SERVIDOR
-const server = http.createServer(
-    async (request: http.IncomingMessage, response: http.ServerResponse) => {
-
-        const [baseUrl, queryString] = request.url?.split("?") ?? ["", ""];
-
-        // listar podcast
-        if (request.method === HttpMethod.GET && baseUrl === Routes.LIST) {
-            await getListEpisodes(request, response);
-        }
-
-        // filtrar podcast
-        if (request.method === HttpMethod.GET && baseUrl === Routes.EPISODE) {
-            await getFilterEpisodes(request, response)
-        }
-    }
-);
+const server = http.createServer(app);
 
 const port = process.env.PORT
 
 server.listen(port, () => {
     console.log(`Server initiated on the port ${port}`);
-})
+});
