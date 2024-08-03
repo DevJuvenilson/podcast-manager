6c5e050aac285f6363562a8e38e0288ddca23930
Daniel Ribeiro <109773063+DevJuvenilson@users.noreply.github.com>
2024-08-03 18:48:28 -0300

    created filter-podcast-model.ts
    created filter to a better handle of errors

--- src/controllers/podcasts-controller.ts
+++ src/controllers/podcasts-controller.ts
@@ -3,6 +3,7 @@ import { serviceFilterEpisodes } from '../services/filter-episodes-service';
 import { serviceListEpisodes } from '../services/list-episodes-service';
 import { ContentType } from '../utils/content-type';
 import { StatusCode } from '../utils/status-code';
+import { FilterPodcastModel } from '../models/filter-podcast-model';
 
 export const getListEpisodes = async (
     req: IncomingMessage, 
@@ -19,8 +20,8 @@ export const getFilterEpisodes = async (
     req: IncomingMessage, 
     res: ServerResponse
 ) => {
-    const content = await serviceFilterEpisodes(req.url);
+    const content: FilterPodcastModel = await serviceFilterEpisodes(req.url);
 
-    res.writeHead(StatusCode.OK, { 'Content-Type': ContentType.JSON });
-    res.end(JSON.stringify(content));
+    res.writeHead(content.statusCode, { 'Content-Type': ContentType.JSON });
+    res.end(JSON.stringify(content.body));
 };
--- src/services/filter-episodes-service.ts
+++ src/services/filter-episodes-service.ts
@@ -1,10 +1,22 @@
+import { FilterPodcastModel } from "../models/filter-podcast-model";
 import { repositoryPodcast } from "../repositories/podcasts-repository";
+import { StatusCode } from "../utils/status-code";
 
-export const serviceFilterEpisodes = async (podcastName: string | undefined) => {
+export const serviceFilterEpisodes = async (
+    podcastName: string | undefined
+): Promise<FilterPodcastModel> => {
+
+    let responseFormat: FilterPodcastModel = {
+        statusCode: 0,
+        body: []
+    };
 
     const queryString = podcastName?.split("?p=")[1] || "";
-
     const data = await repositoryPodcast(queryString);
 
-    return data;
+    responseFormat.statusCode = data.length !== 0 ? StatusCode.OK : StatusCode.NO_CONTENT
+
+    responseFormat.body = data;
+
+    return responseFormat;
 }
